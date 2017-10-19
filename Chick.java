c2679a830a8fb29d55a49f6daec6413ccc27b44e
jechen27 <jechen27@s.sfusd.edu>
2017-10-19 11:33:57 -0700

    Finished

--- Chick.java
+++ Chick.java
@@ -1,4 +1,35 @@
-class Chick //implements Animal 
+class Chick implements Animal 
 {     
-     //your code here
+  private String myType;     
+  private String mySound; 
+  private String mySound2;
+  public Chick(String type, String sound)     
+  {         
+    myType = type;         
+    mySound = sound;      
+  }     
+  public Chick(String type, String sound, String sound2)
+  {
+    myType = type;
+    mySound = sound;
+    mySound2 = sound2;
+  }
+  public Chick()  
+  {         
+    myType = "unknown";          
+    mySound = "unknown";   
+    mySound2 = "unknown";
+  }   
+  public String getSound()
+  {
+    if (mySound2 == null)
+      return mySound;
+    else
+      if (Math.random()<0.5)
+        return mySound2;
+      else
+        return mySound;
+  }    
+    
+  public String getType(){return myType;} 
 }
